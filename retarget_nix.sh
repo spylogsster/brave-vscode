136842fd2b87ac0f522ad4971b309e7a5ea14c70
Your Name <you@example.com>
2021-03-12 11:10:24 +0300

    added nix retarget

---
+++ retarget_nix.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git remote remove origin
+git remote add origin git@git-brave:brave/brave-core.git
+git pull
+git branch --set-upstream-to=origin/master master
+git config user.name "sergey"
+git config user.name "spylogsster@gmail.com"
+git config --global pull.rebase true
