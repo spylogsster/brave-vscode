6b23d1cb2fe7985453401347fd6385ee297f6799
Sergey <rlbssoft@gmail.com>
2021-03-18 08:40:52 +0300

    update

--- retarget_nix.sh
+++ retarget_nix.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git remote remove origin
-git remote add origin git@git-brave:brave/brave-core.git
+git remote add origin git@brave:brave/brave-core.git
 git pull
 git branch --set-upstream-to=origin/master master
-git config user.name "sergey"
-git config user.name "spylogsster@gmail.com"
+git config user.name "Sergey P"
+git config user.email "spylogsster@gmail.com"
 git config --global pull.rebase true
